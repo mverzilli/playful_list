4717fcdcc153bb77bde857d02cc81aed40fb6853
Martin Verzilli <mverzilli@manas.com.ar>
2012-12-01 13:19:52 -0300

    Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlayfulList::Application.config.secret_token = 'be5589ba0618ccaab65d6c3f830e7912e42dab2724a549e06b03f61c1fafa0d4f3c777024eeb1f27e3583059c241709207e80f1aabb33205dce8691dd7bd0890'
