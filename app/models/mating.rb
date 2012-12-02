f1f4dced4cad116ce34b51f3367082965b568df7
Martin Verzilli <mverzilli@manas.com.ar>
2012-12-01 22:03:36 -0300

    bleh

---
+++ app/models/mating.rb
@@ -0,0 +1,17 @@
+class Mating
+  def initialize(level = 1)
+    @level = level
+  end
+
+  def left_column_choices
+  end
+
+  def right_column_choices
+  end
+
+  def question
+  end
+
+  def answer
+  end
+end
