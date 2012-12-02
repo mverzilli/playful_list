08a356deb3a30d15eede4223e8c68bd0015c3e36
Martin Verzilli <mverzilli@manas.com.ar>
2012-12-02 16:00:58 -0300

    All seeds

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
+  attr_accessible :question, :answer, :level_id
+
   belongs_to :question, :class_name => "Choice"
   belongs_to :answer, :class_name => "Choice"
 end
