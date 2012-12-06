12093858dcb296253fbefa252e0d508caf6d1e30
Martin Verzilli <mverzilli@manas.com.ar>
2012-12-06 00:03:45 -0300

    Migration to fix issue #2 directly in prod

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121202193949) do
+ActiveRecord::Schema.define(:version => 20121206025952) do
 
   create_table "choices", :force => true do |t|
     t.string   "image"
